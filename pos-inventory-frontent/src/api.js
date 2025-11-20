56d80f908fe5a1c5ae218b4f5faf7aaf185a2479
Sahlan13 <sahlaibrahimn13@gmail.com>
2025-11-20 22:58:58 +0530

    corrected api.js removed  server deployed url

--- pos-inventory-frontent/src/api.js
+++ pos-inventory-frontent/src/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://pos-inventory-system-1.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 API.interceptors.request.use(config => {
